0653a8ab168342330088980de725fe0b3840fce3
jniestroy <jniestroy@gmail.com>
2020-06-30 13:22:29 -0400

    Fixed Errors with multiple inputs to a node

--- utils.py
+++ utils.py
@@ -65,13 +65,85 @@ def query_stardog(ark):
     Performs the path query
     '''
 
-    results = conn.paths("PATHS START ?x=<"+ ark + "> END ?y VIA ?p",content_type='text/csv')
+    results = conn.paths("PATHS ALL START ?x=<"+ ark + "> END ?y VIA ?p",content_type='text/csv')
     string_csv = io.StringIO(results.decode("utf-8"))
 
     df_eg = pd.read_csv(string_csv, sep=",")
 
     return df_eg
 
+def is_id(string):
+    if 'ark:' in string and len(string) == 46:
+        return True
+    if 'orchid:' in string:
+        return True
+
+    return False
+
+def parse_csv(df):
+    '''
+    Parses the ugly csv returned from stardog
+    to build json evidence graph
+    '''
+
+    context = {'http://www.w3.org/1999/02/22-rdf-syntax-ns#':'@',
+               'http://schema.org/_id':'@id',
+              'http://schema.org/':'',
+               'http://example.org/':'eg:',
+               "https://wf4ever.github.io/ro/2016-01-28/wfdesc/":'wfdesc:'
+              }
+
+    eg = {}
+    current = eg
+    for index, row in df.iterrows():
+
+        #Nan row indicates next item restart at root node
+        if pd.isna(row['x']):
+            current = eg
+            continue
+
+        #cleans tags
+        for key in context:
+            if key in row['p']:
+                row['p'] = row['p'].replace(key,context[key])
+            if key in row['y']:
+                row['y'] = row['y'].replace(key,context[key])
+
+        #If key not already in eg add it
+        #If its an id make it a dict not just an element
+        if row['p'] not in current.keys():
+            if is_id(row['y']):
+                if row['p'] == '@id':
+                    current['@id'] = row['y']
+                else:
+                    current[row['p']] = {'@id':row['y']}
+                    current = current[row['p']]
+            else:
+                current[row['p']] = row['y']
+
+        #If key already exists and its a new id its
+        #not single dict so make it list of dicts
+        elif isinstance(current[row['p']],dict):
+            if current[row['p']]['@id'] == row['y']:
+                current = current[row['p']]
+            else:
+                current[row['p']] = [current[row['p']],{'@id':row['y']}]
+                current = current[row['p']][-1]
+
+        #Look to see if we're moving into an element of the list
+        #ie this is adding on to a known id
+        #or if we're appending a new id to the list
+        elif isinstance(current[row['p']],list):
+            missing = True
+            for dictionary in current[row['p']]:
+                if dictionary['@id'] == row['y']:
+                    current = dictionary
+                    missing = False
+                    break
+            if missing:
+                current[row['p']].append({'@id':row['y']})
+                current = current[row['p']][-1]
+    return eg
 
 def build_evidence_graph(data,clean = True):
     '''
@@ -88,16 +160,14 @@ def build_evidence_graph(data,clean = True):
     trail = []
 
     for index, row in data.iterrows():
-
         if pd.isna(row['x']):
             trail = []
             continue
-
         if clean:
-
             for key in context:
                 if key in row['p']:
                     row['p'] = row['p'].replace(key,context[key])
+                print(row['y'])
                 if key in row['y']:
                     row['y'] = row['y'].replace(key,context[key])
 
@@ -146,7 +216,13 @@ def clean_eg(eg,eg_only = True):
             eg.pop(key, None)
             continue
 
-        if isinstance(eg[key],dict):
+        elif isinstance(eg[key],list):
+
+            for dictionary in eg[key]:
+                if isinstance(dictionary,dict):
+                    dictionary = clean_eg(dictionary)
+
+        elif isinstance(eg[key],dict):
             if len(eg[key]) == 1:
                 #if dict only has one make it no longer a dict
                 #just value instead
@@ -167,7 +243,8 @@ def create_eg(ark):
     '''
 
     df_eg = query_stardog(ark)
-    eg = build_evidence_graph(df_eg)
+    eg = parse_csv(df_eg)
+    #eg = build_evidence_graph(df_eg)
     eg = clean_eg(eg)
 
     return eg
